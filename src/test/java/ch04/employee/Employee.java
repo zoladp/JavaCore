4db7d9153c50e8ab643ac827c7292b2760e3facd
zoladp <zoladp01@gmail.com>
2019-06-14 17:13:13 +0200

    zmiany

--- src/test/java/ch04/employee/Employee.java
+++ src/test/java/ch04/employee/Employee.java
@@ -5,22 +5,26 @@ import java.util.GregorianCalendar;
 
 public class Employee {
     //pola
-    private String name;
+    private final String name;
     private double salary;
+    private double oldSalary;
     private Date hireDay;
+    private static int nextID = 1;
 
     //konstruktor
     public Employee(String n, double s, int year, int month, int day) {
-        name = n;
+        this.name = n;
         salary = s;
+        oldSalary = salary;
         GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
         hireDay = calendar.getTime();
+        //nextID++;
     }
 
+
+
     //metoda
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
     public double getSalary() {
         return salary;
@@ -30,11 +34,15 @@ public class Employee {
         return hireDay;
     }
 
+    public double getOldSalary() { return oldSalary; }
+
+    public static int getNextID() { return nextID++; }
+
     public void raiseSalary(int byPercent) {
-        double raise = salary * byPercent / 100;
-//        salary = salary + raise;
+        double raise = this.salary * byPercent / 100;
         salary += raise;
     }
+
 
     //kolejne metody
 
--- src/test/java/ch04/employee/EmployeeTest.java
+++ src/test/java/ch04/employee/EmployeeTest.java
@@ -6,14 +6,24 @@ public class EmployeeTest {
         Employee[] staff = new Employee[3];
 
         staff[0] = new Employee("Jarosław Rybiński",75000,1987, 12,15);
-        staff[1] = new Employee("Katarzyna Remiszewska", 50000, 1989, 10, 1);
+        staff[1] = new Employee("Katarzyna Remiszewska", 6000, 1989, 10, 1);
         staff[2] = new Employee("Krystyna Kuczyńska", 40000, 1990, 3, 15);
+
+        Employee worker = new Employee("James Bond",100000,1950,1,1);
+        System.out.println(worker.getName()+"/ $"+worker.getSalary());
+
+        //wyswietlenie obecnej pensji przed podwyzka
+       /* for (Employee e : staff)
+            System.out.println("name="+e.getName() + "\t\t\tsalary="+e.getSalary()+"\thireDay="+e.getHireDay());*/
 
         //Zwiekszanie pensji wszystkich pracownikow o 5%
         for (Employee e : staff)
             e.raiseSalary(5);
 
         for (Employee e : staff)
-            System.out.println("name="+e.getName() + " ,salary="+e.getSalary()+" ,hireDay="+e.getHireDay());
+            System.out.println("name="+e.getName() + "\t\t\tsalary="+e.getSalary()+"\tpodwyzka="+(e.getSalary()-e.getOldSalary())+"\t\tthireDay="+e.getHireDay()+"\tnextID:"+e.getNextID());
+
+        System.out.println(Math.PI);
+
     }
 }
